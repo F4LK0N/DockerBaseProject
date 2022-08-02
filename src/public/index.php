cffe3d16db67664e7009fbfc6dea807a247bfce0
OtavioSoria <devel_17@autoavaliar.com.br>
2022-08-02 20:29:49 -0300

    Index example content

--- src/public/index.php
+++ src/public/index.php
@@ -1,7 +1,16 @@
 <?php
 
-print"Teste: ". php_ini_loaded_file() . "<br>";
+var_dump([
+    "String string string",
+    true,
+    1234,
+    [1,2,3],
+    (object)[
+        "attrb1"=>3,
+        "attrb2"=>6,
+    ]
+]);
 
-var_dump([1=>123]);
+xdebug_info();
 
 phpinfo();
